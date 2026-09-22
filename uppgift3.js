ab72abc9d53bde58c350ea97b17d427fccac92a8
Linneah <liol2603@student.miun.se>
2026-09-22 17:04:05 +0200

    får koden att fungera men just här gäller första else if för alla över 65 så else körs aldrig

--- uppgift3.js
+++ uppgift3.js
@@ -1,7 +1,11 @@
 "use strict";
 
-let age = 38;
+let age = 77;
 
 if (age < 18) {
-  console.log(`Du är ${age} år gammal och får köpa barnbiljett!`);
+  console.log(`Du är ${age} år gammal och kan köpa barnbiljett!`);
+} else if (18 <= age < 65) {
+  console.log(`Du är ${age} år gammal och kan inte köpa barnbiljett!`);
+} else {
+  console.log(`Du är ${age} år gammal och kan köpa seniorbiljett!`);
 }

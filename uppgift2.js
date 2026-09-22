83178dceab4c7ec8d16d50d31a0f603c6cddf4cc
Linneah <liol2603@student.miun.se>
2026-09-22 14:59:57 +0200

    förfinat kommentar

--- uppgift2.js
+++ uppgift2.js
@@ -3,7 +3,7 @@
 let produktPris = 100; //Produktens pris
 let produktAntal = 3; //Antal produkter
 
-// Beräkningen visar totalpriset för produkterna och vad det totala priset blir inklusive 25% moms
+// Beräkningen visar totalpriset för antalet produkter och vad det totala priset blir inklusive 25% moms
 console.log(`Pris: ${produktPris}kr`);
 console.log(`Antal: ${produktAntal}st`);
 console.log(`Totalt: ${produktPris * produktAntal}kr`);

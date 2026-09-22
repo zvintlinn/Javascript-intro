440e742c95e48719a2d2899d6aebff40d867b041
Linneah <liol2603@student.miun.se>
2026-09-22 18:20:42 +0200

    lagt till kommentarer

--- uppgift3.js
+++ uppgift3.js
@@ -1,11 +1,11 @@
 "use strict";
 
-let age = 67;
+let age = 67; //resenärens ålder
 
 if (age < 18) {
-  console.log(`Du är ${age} år gammal och kan köpa barnbiljett!`);
+  console.log(`Du är ${age} år gammal och kan köpa barnbiljett!`); //Under 18 = barnbiljett
 } else if (age < 65) {
-  console.log(`Du är ${age} år gammal och måste köpa ordinarie biljett!`);
+  console.log(`Du är ${age} år gammal och måste köpa ordinarie biljett!`); //18-64 = vuxenbiljett
 } else {
-  console.log(`Du är ${age} år gammal och kan köpa seniorbiljett!`);
+  console.log(`Du är ${age} år gammal och kan köpa seniorbiljett!`); //65+ = seniorbiljett
 }

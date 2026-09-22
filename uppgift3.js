39505c91b97c4a1f0b98bef89ed788a8815d82dd
Linneah <liol2603@student.miun.se>
2026-09-22 17:08:54 +0200

    kom på att koden körs uppifrån och ned, ändrat villkor till age < 65 på else if

--- uppgift3.js
+++ uppgift3.js
@@ -1,11 +1,11 @@
 "use strict";
 
-let age = 77;
+let age = 67;
 
 if (age < 18) {
   console.log(`Du är ${age} år gammal och kan köpa barnbiljett!`);
-} else if (18 <= age < 65) {
-  console.log(`Du är ${age} år gammal och kan inte köpa barnbiljett!`);
+} else if (age < 65) {
+  console.log(`Du är ${age} år gammal och måste köpa ordinarie biljett!`);
 } else {
   console.log(`Du är ${age} år gammal och kan köpa seniorbiljett!`);
 }

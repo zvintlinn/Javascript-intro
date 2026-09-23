c10f7b4ff8b7dcd85fdae459e7117a6191dbabaa
Linneah <liol2603@student.miun.se>
2026-09-23 09:07:16 +0200

    kommenterat kod

--- uppgift4.js
+++ uppgift4.js
@@ -1,6 +1,10 @@
 "use strict";
 
+// Loopen körs bara tom. tal 20. När i/2 inte får någon rest är talet jämnt och visas i konsolen.
+
+//Loop
 for (let i = 1; i <= 20; i++) {
+  //villkor för vad som visas i konsol
   if (i % 2 === 0) {
     console.log(i);
   }

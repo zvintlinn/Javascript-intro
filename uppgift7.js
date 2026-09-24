032469330d4b6367b0ab392913fc7b15850dc495
Linneah <liol2603@student.miun.se>
2026-09-24 22:00:47 +0200

    tagit bort ett överflödigt par måsvingar som gav mig fel

--- uppgift7.js
+++ uppgift7.js
@@ -4,10 +4,8 @@ let numbers = [2, 6, 3, 5, 8, 9];
 
 let sumOfNumbers = (array) => {
   let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum = sum + numbers[i];
-    return sum;
-  }
+  for (let i = 0; i < numbers.length; i++) sum = sum + numbers[i];
+  return sum;
 };
 
 console.log(sumOfNumbers(numbers));

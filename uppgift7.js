1d0540882d2b0e0da20bc7250e36eec08f6cdbb4
Linneah <liol2603@student.miun.se>
2026-09-24 23:09:40 +0200

    kommenterat kod

--- uppgift7.js
+++ uppgift7.js
@@ -3,9 +3,11 @@
 let numbers = [10, 100, 100, 200, 300, 650];
 
 let sumOfNumbers = (array) => {
-  let sum = 0;
+  //funktion som tar emot array
+  let sum = 0; //variabel som ska lagra summan efter varje loop-varv
   for (let i = 0; i < numbers.length; i++) sum = sum + numbers[i];
-  return sum;
+  //första värdet i array adderas till sum som då är 0 och lagras i sum till nästa varv då nästa värde används
+  return sum; //totalen returneras i slutet
 };
 
 console.log(sumOfNumbers(numbers));

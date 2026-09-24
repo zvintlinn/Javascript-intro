c9cae617046521e6b3b29444b8b6a410b85e7a07
Linneah <liol2603@student.miun.se>
2026-09-24 20:59:13 +0200

    använder funktionen två gånger till med andra värden

--- uppgift6.js
+++ uppgift6.js
@@ -3,3 +3,5 @@
 let calculateArea = (width, height) => width * height;
 
 console.log(`Arean är ${calculateArea(3, 2)} m^2`);
+console.log(`Arean är ${calculateArea(18, 30)} m^2`);
+console.log(`Arean är ${calculateArea(27, 103)} m^2`);

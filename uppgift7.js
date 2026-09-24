727d2a17bbad22bb0a9a2f624bbdca02fc691fb6
Linneah <liol2603@student.miun.se>
2026-09-24 23:10:44 +0200

    fixade en kommentar till två rader istället för en

--- uppgift7.js
+++ uppgift7.js
@@ -6,7 +6,8 @@ let sumOfNumbers = (array) => {
   //funktion som tar emot array
   let sum = 0; //variabel som ska lagra summan efter varje loop-varv
   for (let i = 0; i < numbers.length; i++) sum = sum + numbers[i];
-  //första värdet i array adderas till sum som då är 0 och lagras i sum till nästa varv då nästa värde används
+  /*första värdet i array adderas till sum som då är 0 och 
+  lagras i sum till nästa varv då nästa värde används*/
   return sum; //totalen returneras i slutet
 };
 

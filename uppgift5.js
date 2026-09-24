22fac6f369950767f94cad862525a4a42294517f
Linneah <liol2603@student.miun.se>
2026-09-24 16:47:51 +0200

    kommenterat kod

--- uppgift5.js
+++ uppgift5.js
@@ -1,11 +1,11 @@
 "use strict";
 
 let dishes = ["carbonara", "pizza", "hamburgare", "räksallad", "lasagne"];
-console.log(`1. ${dishes}`);
-console.log(`2. ${dishes[0]}`);
-console.log(`3. ${dishes[4]}`);
+console.log(`1. ${dishes}`); //Upg 1
+console.log(`2. ${dishes[0]}`); //Upg 2
+console.log(`3. ${dishes[4]}`); //Upg 3
 
-dishes.push("Köttsoppa");
-dishes.shift();
+dishes.push("Köttsoppa"); //Upg 4
+dishes.shift(); //Upg 5
 
-console.log(`6. ${dishes}`);
+console.log(`6. ${dishes}`); //Upg 6

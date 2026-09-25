488f55540391015b1f5199cef9b3492e4aee475d
Linneah <liol2603@student.miun.se>
2026-09-25 10:28:09 +0200

    lagt till funktion som ska skriva ut objekten

--- uppgift8.js
+++ uppgift8.js
@@ -4,4 +4,9 @@ function book(title, author, year) {
   this.title = title;
   this.author = author;
   this.year = year;
+  this.presentation = function () {
+    console.log(`Titel: ${this.title}`);
+    console.log(`Författare: ${this.author}`);
+    console.log(`Utgivningsår: ${this.year}`);
+  };
 }

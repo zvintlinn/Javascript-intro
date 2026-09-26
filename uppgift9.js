9e9d82283fa08683ee5c15bd659c58e85c300717
Linneah <liol2603@student.miun.se>
2026-09-26 13:05:37 +0200

    lagt till kommentarer

--- uppgift9.js
+++ uppgift9.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//array med tre personer
 const people = [
   { name: "Linneah", age: 25, city: "Östersund" },
   {
@@ -13,9 +14,10 @@ const people = [
     city: "Göteborg",
   },
 ];
-
+//funktion som tar emot array
 function giveInfo(people) {
   for (let i = 0; i < people.length; i++) {
+    //loop genom arrayen, if-sats avgör om person är myndig eller ej
     if (people[i].age < 18) {
       console.log(
         `${people[i].name} bor i ${people[i].city} och är inte myndig.`,
@@ -26,4 +28,4 @@ function giveInfo(people) {
   }
 }
 
-giveInfo(people);
+giveInfo(people); //anropar funktion
